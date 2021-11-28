df6b3412fbfe0c0b354a1f1e67fbfe13c167055c
lrglvz <galvezlaramarie@gmail.com>
2021-11-28 16:07:02 +0800

    all the criteria for creating password are met.

--- password.py
+++ password.py
@@ -10,9 +10,23 @@
 displayIntro = print("Welcome to Registration! You are about to create your own account. Please sign up for your Username and Password.")
 askUsername = input("Username: ")
 askPassword = input("Password: ")
+capLet = False 
+numberChar = False
+specialChar = False
 
-if len(askPassword) > 15:
-    print("Valid")
+if len(askPassword) >= 15: #if-else (a. Greater than 15 letters)
+    for char in askPassword: #loop statement
+        if char.isalpha(): 
+            if char.isupper(): # (b. Have at least one capital letter)
+                capLet = True
+        elif char.isnumeric(): # (c. Have at least one number)
+            numberChar = True
+        else:
+            specialChar = True # (d. Have at least one special char)
+    if capLet == True and numberChar == True and specialChar == True:
+        result = "Valid"
+    else:
+        result = "Invalid"
 else: 
-    print("Invalid!")
-
+    result = "Invalid"
+print(result)
